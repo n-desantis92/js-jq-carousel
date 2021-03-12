cfee4a146a5479c94ddabc019c2d087f05ac6054
n-desantis92 <dsnwebdev@gmail.com>
2021-03-12 18:56:43 +0100

    ora bullet e img si spostano insieme solo dalle frecce

--- main.js
+++ main.js
@@ -14,42 +14,51 @@ $(function () {
 
   $(".fa-circle").click(
     function () {
-      var circleActive = $(".fa-circle");
-
-      circleActive.removeClass("active");
-      $(this).addClass("active");
-      
-
+      bulletActive();
     }
-  )
-
-
-
+  );
 
 
 });
 
 function preImg() {
   var imgActive = $(".images img.active");
+  var circleActive = $(".nav .fas.fa-circle.active");
 
   imgActive.removeClass("active");
+  circleActive.removeClass("active");
 
-  if (imgActive.hasClass("first") == true) {
+  if (imgActive.hasClass("first") == true && circleActive.hasClass("first") == true) {
     $(".images img.last").addClass("active")
+    $(".nav .fas.fa-circle.last").addClass("active");
+
   } else {
     imgActive.prev().addClass("active");
+    circleActive.prev().addClass("active");
+
   }
 };
 
 
 function nextImg() {
   var imgActive = $(".images img.active");
+  var circleActive = $(".nav .fas.fa-circle.active");
 
   imgActive.removeClass("active");
+  circleActive.removeClass("active");
 
-  if (imgActive.hasClass("last") == true) {
-    $(".images img.first").addClass("active")
+  if (imgActive.hasClass("last") == true && circleActive.hasClass("last") == true) {
+    $(".images img.first").addClass("active");
+    $(".nav .fa-circle.first").addClass("active");
   } else {
     imgActive.next().addClass("active");
+    circleActive.next().addClass("active");
+
     }
 };
+
+// function bulletActive() {
+//   $(this).addClass("active");
+//
+//
+// }
