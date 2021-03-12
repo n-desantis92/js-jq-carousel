5315a26767a9d2446295e3aa442e8e3a3dba382a
n-desantis92 <dsnwebdev@gmail.com>
2021-03-12 17:43:02 +0100

    aggiunto click all bullet

--- main.js
+++ main.js
@@ -2,31 +2,54 @@ $(function () {
 
   $(".prev").click(
     function () {
+      preImg();
+    }
+  );
 
-      var imgActive = $(".images img.active");
+  $(".next").click(
+    function () {
+      nextImg();
+    }
+  );
 
-      imgActive.removeClass("active");
-
-      if (imgActive.hasClass("first") == true) {
-        $(".images img.last").addClass("active")
+  $(".fas.fa-circle").click(
+    function () {
+      var circleActive = $("i.fa-circle");
+      if (circleActive.hasClass("active") == true) {
+        circleActive.removeClass("active");
       } else {
-        imgActive.prev().addClass("active");
-        }
+        $(this).addClass("active");
+      }
     }
   )
 
 
-    $(".next").click(
-      function () {
-        var imgActive = $(".images img.active");
 
-        imgActive.removeClass("active");
 
-        if (imgActive.hasClass("last") == true) {
-          $(".images img.first").addClass("active")
-        } else {
-          imgActive.next().addClass("active");
-          }
-      }
-    )
-})
+
+});
+
+function preImg() {
+  var imgActive = $(".images img.active");
+
+  imgActive.removeClass("active");
+
+  if (imgActive.hasClass("first") == true) {
+    $(".images img.last").addClass("active")
+  } else {
+    imgActive.prev().addClass("active");
+  }
+};
+
+
+function nextImg() {
+  var imgActive = $(".images img.active");
+
+  imgActive.removeClass("active");
+
+  if (imgActive.hasClass("last") == true) {
+    $(".images img.first").addClass("active")
+  } else {
+    imgActive.next().addClass("active");
+    }
+};
