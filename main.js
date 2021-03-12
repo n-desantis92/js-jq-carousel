de0c982a05293f6f69967cb31fa5e36c71db832d
n-desantis92 <dsnwebdev@gmail.com>
2021-03-12 15:46:16 +0100

    aggiunto evento al click delle freccie

--- main.js
+++ main.js
@@ -2,17 +2,31 @@ $(function () {
 
   $(".prev").click(
     function () {
-      var attivazione = $(".images img.active");
-      attivazione.removeClass("active");
-      attivazione.next().addClass("active");
 
+      var imgActive = $(".images img.active");
+
+      imgActive.removeClass("active");
+
+      if (imgActive.hasClass("first") == true) {
+        $(".images img.last").addClass("active")
+      } else {
+        imgActive.prev().addClass("active");
+        }
     }
   )
 
 
     $(".next").click(
       function () {
+        var imgActive = $(".images img.active");
 
+        imgActive.removeClass("active");
+
+        if (imgActive.hasClass("last") == true) {
+          $(".images img.first").addClass("active")
+        } else {
+          imgActive.next().addClass("active");
+          }
       }
     )
 })
